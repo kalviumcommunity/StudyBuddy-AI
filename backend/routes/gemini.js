b7ac6dc00cc9e7250b13e132407f47cf723175f3
Ayishath Nahda <ayishathnahda.9i@gmail.com>
2025-08-22 09:06:00 +0530

    dynamic-prompting done

--- backend/routes/gemini.js
+++ backend/routes/gemini.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { zeroShot, oneShot, systemPrompt, multiShot } = require("../controllers/geminiController");
+const { zeroShot, oneShot, systemPrompt, multiShot, dynamicPrompt } = require("../controllers/geminiController");
 const router = express.Router();
 
 router.post("/zero-shot", zeroShot);
@@ -9,5 +9,7 @@ router.post("/one-shot", oneShot);
 router.post("/system-user", systemPrompt);
 
 router.post("/multi-shot",multiShot);
+
+router.post("/dynamic", dynamicPrompt);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,7 @@ app.use("/api/auth", authRoutes);
 const geminiRoutes = require("./routes/gemini");
 app.use("/api/gemini", geminiRoutes);
 
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on: http://localhost:${PORT}`));
